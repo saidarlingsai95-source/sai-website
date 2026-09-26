23e720141b24e2e7d47dd54af38052e497162f3c
saidarlingsai95-source <saidarlingsai95@gmail.com>
2026-09-26 17:35:39 +0530

    Refactor Flask app to remove SocketIO support
    Removed SocketIO integration and updated the main route function name from 'index' to 'home'. Changed the server port from 10000 to 5000.

--- app.py
+++ app.py
@@ -1,29 +1,12 @@
 from flask import Flask, render_template
-from flask_socketio import SocketIO, emit
 import os
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'sai-chat-secret'
-socketio = SocketIO(app, cors_allowed_origins="*")
-users = {}
 
 @app.route('/')
-def index():
+def home():
     return render_template('index.html')
 
-@socketio.on('join')
-def handle_join(data):
-    users[data['sid']] = data['username']
-    emit('user_joined', {'username': data['username']}, broadcast=True)
-
-@socketio.on('message')
-def handle_message(data):
-    emit('new_message', {'username': data['username'], 'message': data['message']}, broadcast=True)
-
-@socketio.on('disconnect')
-def handle_disconnect():
-    users.pop(str(id), None)
-
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 10000))
-    socketio.run(app, host='0.0.0.0', port=port)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
